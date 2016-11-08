2886cf2cf4a3428960d32ee7cb168ad950a0e681
Randel <homankids@gmail.com>
2016-11-08 16:21:35 -0600

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!, A Change from github')
 })
 
+app.get('/message', function (req, res) {
+  res.send('World, Hello')
+})
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
