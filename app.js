1652ff16815fb995684a9ae116d282ea9049b046
Randel <homankids@gmail.com>
2016-11-07 20:34:26 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello World!, A Change from github')
 })
 
 app.listen(3000, function () {
